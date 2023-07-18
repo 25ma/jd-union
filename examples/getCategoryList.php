5f347ab0ddb6f2ae3d63ef0f49b92867fb4095bf
25ma <25mateam@gmail.com>
2023-07-18 11:28:37 +0800

    modify examples

--- examples/getCategoryList.php
+++ examples/getCategoryList.php
@@ -1,14 +1,12 @@
 <?php
 
-// require('../src/Client.php');
 require('../vendor/autoload.php');
 
 //测试demo
 $c = new \I25ma\JdUnion\Client;
-$c->appKey = "991f86fe9af848b49f431cede6ee4fda";
-$c->appSecret = "91263ddc5fbb49e4a15b2ee3566afcdb";
+$c->appKey = "your appKey";
+$c->appSecret = "your appSecret";
 // $c->accessToken = "1f1d3048-220a-484d-ad93-f3808d9aacc1";
-// $c->serverUrl = "http://gw.api.360buy.net/routerjson";
 $req = new \I25ma\JdUnion\request\UnionOpenCategoryGoodsGetRequest();
 $req->setReq(["parentId" => 0, 'grade' => 0]);
 $resp = $c->execute($req);
